532fd40d078f9c3794cbea9d766561ca2aff938b
Vladislav <studentredin323@gmail.com>
2021-09-05 22:17:41 +0300

    Lesson_01 HW Complete

---
+++ Lesson_01/js/main.js
@@ -0,0 +1,19 @@
+const products = [
+    {id: 1, image: '<img src = "./img/notebook.jpg">', title: 'Notebook', price: 1000},
+    {id: 2, image: '<img src = "./img/mouse.jpg">', title: 'Mouse', price: 100},
+    {id: 3, image: '<img src = "./img/keyboard.jpg">', title: 'Keyboard', price: 250},
+    {id: 4, image: '<img src = "./img/gamepad.jpeg">', title: 'Gamepad', price: 150},
+  ];
+  
+  
+  
+  const renderProduct = (image, title, price) => {
+    return `<div class="product-item">${image}<h3>${title}</h3><p>${price}</p><button class="by-btn">Добавить</button></div>`;
+  };
+  
+  const renderProducts = list =>{
+    document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item.image, item.title, item.price)).join(''));
+  }
+  
+  renderProducts(products);
+  
