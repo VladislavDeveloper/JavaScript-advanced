6e51fcb6ad74e601c507e182fc16b3cc77963e52
VladislavDeveloper <83352504+VladislavDeveloper@users.noreply.github.com>
2021-09-12 15:47:30 +0300

    Complete

--- Lesson_02/js/script.js
+++ Lesson_02/js/script.js
@@ -1,4 +1,5 @@
-
+//1. Добавьте пустые классы для корзины товаров и элемента корзины товаров. Продумайте, какие методы понадобятся для работы с этими сущностями.
+//2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
 
 
 //Класс для карточки товара
@@ -43,7 +44,80 @@ class GoodsList {
         })
         document.querySelector('.products').innerHTML = list;
     }
+    //Метод для подсчета суммарной суммы всех товаров
+    totalGoodsPrice() {
+        let sum = 0;
+        this.goods.forEach(good => {
+            sum += good.price;
+        })
+        console.log(sum);
+    }
+}
+
+
+//Класс для корзины товаров
+class Cart {
+    constructor() {
+        this.goodsInCart = [];
+        this.goods = [
+            { id: 1, image: './img/notebook.jpg', title: 'Notebook', price: 1000 },
+            { id: 2, image: './img/mouse.jpg', title: 'Mouse', price: 100 },
+            { id: 3, image: './img/keyboard.jpg', title: 'Keyboard', price: 250 },
+            { id: 4, image: './img/gamepad.jpeg', title: 'Gamepad', price: 150 },
+        ]
+    }
+    //Метод для добавления товара в корзину
+    addToCart() {
+        this.goodsInCart.push(this.goods[1], this.goods[3]);//добавили условно два элемента в корзину mouse и gamepad
+        console.log(this.goodsInCart);
+    }
+    //Метод для удаления товара из корзины
+    deleteCartItem() {
+        this.goodsInCart.splice(0, 1);
+        console.log(this.goodsInCart);
+    }
+    //Метод для подсчета итоговой суммы товаров, добавленных пользователем в корзину
+    totalCartPrice() {
+        let sum = 0;
+        this.goodsInCart.forEach(good => {
+            sum += good.price
+        })
+        console.log(sum);
+    }
+    //Метод для отрисовки товаров в корзине
+    render() {
+        const cartBox = document.querySelector('.productBox');
+        for (let product of this.goodsInCart) {
+            const cartObj = new ProductInCart(product);
+            cartBox.insertAdjacentHTML('beforeend', cartObj.render());
+        }
+    }
+}
+
+//Класс карточки товара для корзины
+class ProductInCart {
+    constructor(product) {
+        this.title = product.title;
+        this.price = product.price;
+    }
+    render() {
+        return `<h4>${this.title} ${this.price}</h4>`;
+    }
+}
+
+//Функция для отрисовки и отображения корзины в браузере
+showCart = () => {
+    display = document.querySelector('.cart')
+    cart.render();
+    display.style.display = 'block';
 }
 
 const list = new GoodsList();
 list.render();
+list.totalGoodsPrice(); //Проверка работоспособности метода totalGoodsPrice, результат будет выведен в консоль
+
+const cart = new Cart();
+cart.addToCart();//Проверка симуляции добовления товара в корзину, вывод в консоль элементов массива goodsInCart
+cart.deleteCartItem();//Проверка метода удаление товара из корзины
+cart.totalCartPrice();//Проверка подсчета итоговой суммы корзины
+const cartBtn = document.querySelector('.btn-cart').addEventListener('click', showCart);//Для проверки отрисовки корзины нв браузере необъодимо кликнуть по кнопке "корзина"
